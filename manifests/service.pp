5783edd0d6cddc1cdd0430a95b4d034fbadb7596
Matt Heath <matt@mattheath.com>
2013-07-18 11:59:54 +0100

    Notify service if executable changes, also ensure the repo has been cloned and log dir exists

--- manifests/service.pp
+++ manifests/service.pp
@@ -12,17 +12,19 @@ class statsd::service {
     group   => 'wheel',
     notify  => Service['dev.statsd'],
     require => [
-      File[$statsd::config::executable],
+      File[$statsd::config::logdir],
+      Repository[$statsd::config::libdir],
     ]
   }
 
   file { $statsd::config::executable:
     content => template('statsd/statsd.erb'),
     mode    => '0755'
+    notify  => Service['dev.statsd'],
   }
 
   service { 'dev.statsd':
-    ensure    => running
+    ensure  => running
   }
 
   service { 'com.boxen.statsd':
